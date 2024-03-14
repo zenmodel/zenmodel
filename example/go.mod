433124e1ca948de4a012a0758452dd1a470c414b
Clay Zhang <clay.zhang@outlook.com>
2024-03-14 21:31:12 +0800

    feat(example): update simple example

--- example/go.mod
+++ example/go.mod
@@ -5,9 +5,14 @@ go 1.21.0
 require github.com/zenmodel/zenmodel v0.1.0
 
 require (
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/dgraph-io/ristretto v0.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	k8s.io/apimachinery v0.29.2 // indirect
 	k8s.io/client-go v0.29.2 // indirect
--- example/simple/simple.go
+++ example/simple/simple.go
@@ -8,66 +8,58 @@ import (
 )
 
 func main() {
-	//brainPrint := zenmodel.NewBrainPrint()
-	//neuron1 := brainPrint.AddNeuron(fn1)
-	//neuron2 := brainPrint.AddNeuron(fn2)
-	//neuron3 := brainPrint.AddNeuron(fn3)
-	//neuron4 := brainPrint.AddNeuron(fn4)
-	//brainPrint.AddNeuronWithProcessor()
-	//
-	//entry1, err := brainPrint.AddEntryLink(neuron1)
-	//_, err = brainPrint.AddLink(neuron1, neuron2)
-	//_, err = brainPrint.AddLink(neuron1, neuron3)
-	//link24, err := brainPrint.AddLink(neuron2, neuron4)
-	//link34, err := brainPrint.AddLink(neuron3, neuron4)
-	//err = brainPrint.AddTriggerGroup(neuron2, link24, link34)
-	//
-	//brain := brainPrint.Build(zenmodel.WithLocalMaintainer()) // Build 	参数默认是 local, 就是使用本地 channel&goroutine 去维护 brain. 当然也需要支持对接远端 MQ, 对接远端（grpc）维护用的计算资源/
-	//
-	//brain.SetContext()
-	//brain.SetContextUnsafe()
-	//brain.SetContextField()
-	//brain.SetContextFieldunsafe()
-	//brain.GetContext()
-	//brain.GetOutput() // 如果是 stream , 则是 stream channel 中的元素拼接而成的结果
-	//brain.WatchOutput()- > chan
-	//
-	//
-	//brain.GetStatus()
-	//
-	//
-	//// 触发所有 Entry Links
-	//brain.Entry()
-	//// 触发指定 links。可以从任意 links 来激活 brain
-	//brain.TrigLinks()
-
-	// goroutine 在 trigLinks 是ensure 开启,在 brain stop chan 收到消息时关闭
-
-	fn1 := func(b zenmodel.Brain) error {
-		fmt.Println("-----------> fn1")
-		return nil
-	}
-	fn2 := func(b zenmodel.Brain) error {
-		fmt.Println("-----------> fn2")
-		return nil
-	}
-
 	bp := zenmodel.NewBrainPrint()
 	n1 := bp.AddNeuron(fn1)
 	n2 := bp.AddNeuron(fn2)
 	_, err := bp.AddLink(n1, n2)
 	if err != nil {
-		fmt.Printf("add link error --------> %s\n", err)
+		fmt.Printf("add link error: %s\n", err)
 		return
 	}
 	_, err = bp.AddEntryLink(n1)
 	if err != nil {
-		fmt.Printf("add entry link error --------> %s\n", err)
+		fmt.Printf("add entry link error: %s\n", err)
 		return
 	}
+
+	//bp.AddLinkToConductGroup()
+	//bp.AddTriggerGroup()
 	brain := bp.Build()
 
 	brain.Entry()
 
 	time.Sleep(10 * time.Second)
+
+	name, found := brain.GetMemory("name")
+	if !found {
+		fmt.Println("name not found")
+		return
+	}
+	fmt.Printf("result: my name is %s.\n", name)
+	//brain.GetStatus()
+}
+
+func fn1(b zenmodel.Brain) error {
+	fmt.Println("start fn1 ..............")
+
+	if err := b.SetMemory("name", "Clay"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func fn2(b zenmodel.Brain) error {
+	fmt.Println("start fn2 ..............")
+
+	firstName, found := b.GetMemory("name")
+	if !found {
+		return nil
+	}
+
+	name := firstName.(string) + " Zhang"
+	if err := b.SetMemory("name", name); err != nil {
+		return err
+	}
+	return nil
 }
